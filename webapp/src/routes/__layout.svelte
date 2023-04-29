323d9e2662c1ff53f17993e2aacaa6a06bb279c2
Robin Granger <robin.granger@analogway.com>
2023-04-29 10:20:12 +0200

    ft(webapp): copy code from other project

---
+++ webapp/src/routes/__layout.svelte
@@ -0,0 +1,9 @@
+<script>
+	import '../app.css';
+	import { init } from '$lib/ws';
+	import { onMount } from 'svelte';
+
+	onMount(init);
+</script>
+
+<slot />
---
+++ webapp/src/routes/index.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	import Paper, { Title, Subtitle, Content } from '@smui/paper';
+	import Button from '@smui/button';
+	import { send } from '$lib/ws';
+
+	const handleCreateRoom = () => {
+		send({ type: 'CreateRoom' });
+	};
+
+	const handleJoinRoom = () => {
+		console.log('join room');
+	};
+</script>
+
+<main class="h-screen flex flex-col p-10">
+	<h1 class="text-8xl text-center">
+		<span class="text-red-500">S</span>
+		<span class="text-yellow-500">K</span>
+		<span class="text-green-500">Y</span>
+		<span class="text-cyan-500">J</span>
+		<span class="text-blue-500">O</span>
+		<span>&nbsp;</span>
+		<span class="text-red-500">U</span>
+		<span class="text-yellow-500">N</span>
+		<span class="text-green-500">L</span>
+		<span class="text-cyan-500">I</span>
+		<span class="text-blue-500">M</span>
+		<span class="text-red-500">I</span>
+		<span class="text-yellow-500">T</span>
+		<span class="text-green-500">E</span>
+		<span class="text-cyan-500">D</span>
+	</h1>
+
+	<div class="flex-1 flex justify-center	items-center">
+		<Paper class="flex-1 flex flex-col max-w-lg">
+			<Button class="mb-2" on:click={handleCreateRoom} variant="raised">Create Room</Button>
+			<Button on:click={handleJoinRoom} variant="raised" disabled>Join Room</Button>
+		</Paper>
+	</div>
+</main>
