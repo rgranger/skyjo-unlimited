06075f6b1ca99f2cd8a9ebf859093d40f224c43d
RGR <rg.robingranger@gmail.com>
2023-05-08 21:48:14 +0200

    feat(webapp): ameliore page d'accueil

--- webapp/src/routes/+page.svelte
+++ webapp/src/routes/+page.svelte
@@ -1,20 +1,35 @@
+<script>
+	import Card from '@smui/card'
+	import Button from '@smui/button'
+
+	const handleCreateNewRoom = () => {
+		console.log('create new room')
+	}
+
+	const handleJoinRoom = () => {
+		console.log('join room')
+	}
+</script>
+
 <svelte:head>
 	<title>Skyjo Unlimited</title>
 	<meta name="description" content="Skyjo Unlimited" />
 </svelte:head>
 
 <section>
-	<h1>SKYJO UNLIMITED</h1>
-	<button>Create New Room</button>
-	<button>Join Room</button>
+	<Card variant="raised" class="gap-3 p-6 ">
+		<h1>SKYJO UNLIMITED</h1>
+		<Button on:click={handleCreateNewRoom} variant="raised">Create New Room</Button>
+		<Button on:click={handleJoinRoom} variant="outlined" color="secondary">Join Room</Button>
+	</Card>
 </section>
 
 <style>
 	section {
+		flex: 0.9;
 		display: flex;
 		flex-direction: column;
 		justify-content: center;
 		align-items: center;
-		flex: 0.6;
 	}
 </style>
